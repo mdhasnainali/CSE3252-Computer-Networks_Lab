a3d2e1f85631ead20e94b13372ef82c6a3ff0541
Md. Hasnain Ali <mdhasnainali.01@gmail.com>
2024-11-13 10:40:23 +0600

    Update SMTP_MAIL_SENDER.java

--- Java Socket Programming/SMTP-Mail-Send/SMTP_MAIL_SENDER.java
+++ Java Socket Programming/SMTP-Mail-Send/SMTP_MAIL_SENDER.java
@@ -13,7 +13,7 @@ class SMTP_MAIL_SENDER {
     String pass = "QW1hciBoYXRlIGVrIHJhcyBwYXA=";
     
     String username =new String(Base64.getEncoder().encode(user.getBytes()));
-    String password =pass;// new String(Base64.getEncoder().encode(pass.getBytes()));
+    String password =new String(Base64.getEncoder().encode(pass.getBytes()));
     SSLSocket s = (SSLSocket) SSLSocketFactory.getDefault().createSocket("smtp.gmail.com", 465);
     dos = new DataOutputStream(s.getOutputStream());
     br = new BufferedReader(new InputStreamReader(s.getInputStream()));
